d3c9796dec971a65b0dbc75206f0fde915995928
李 鍾杰 <jklee@gameon.co.jp>
2017-06-14 16:17:37 +0900

    add randomise

--- Internal/Extension/EnumerableExtension.cs
+++ Internal/Extension/EnumerableExtension.cs
@@ -1,40 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace UniFramework.Extension
 {
 
-	public static class EnumerableExtension
-	{
-		public static IEnumerable<T> Empty<T> ()
-		{
-			return new T[0];
-		}
+    public static class EnumerableExtension
+    {
+        public static IEnumerable<t> Randomize<t>(this IEnumerable<t> target)
+        {
+            Random r = new Random();
 
-		public static IEnumerable<T> LazyEach<T> (this IEnumerable<T> source, Action<T> fn)
-		{
-			foreach (var item in source) {
-				fn.Invoke (item);
+            return target.OrderBy(x => (r.Next()));
+        }
 
-				yield return item;
-			}
-		}
+        // public static IEnumerable<T> Empty<T> ()
+        // {
+        // 	return new T[0];
+        // }
 
-		public static void Each<T> (this IEnumerable<T> source, Action<T> fn)
-		{
-			foreach (var item in source) {
-				fn.Invoke (item);
-			}
-		}
+        // public static IEnumerable<T> LazyEach<T> (this IEnumerable<T> source, Action<T> fn)
+        // {
+        // 	foreach (var item in source) {
+        // 		fn.Invoke (item);
 
-		public static void Each<T> (this IEnumerable<T> source, Action<T, int> fn)
-		{
-			int index = 0;
+        // 		yield return item;
+        // 	}
+        // }
 
-			foreach (T item in source) {
-				fn.Invoke (item, index);
-				index++;
-			}
-		}
-	}
+        // public static void Each<T> (this IEnumerable<T> source, Action<T> fn)
+        // {
+        // 	foreach (var item in source) {
+        // 		fn.Invoke (item);
+        // 	}
+        // }
+
+        // public static void Each<T> (this IEnumerable<T> source, Action<T, int> fn)
+        // {
+        // 	int index = 0;
+
+        // 	foreach (T item in source) {
+        // 		fn.Invoke (item, index);
+        // 		index++;
+        // 	}
+        // }
+    }
 }
